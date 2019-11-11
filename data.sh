e48511db80b979455d0a025a53f166d461c7e802
YuriiMO <57433777+YuriiMO@users.noreply.github.com>
2019-11-11 16:49:58 +0200

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
+# запускаем службу mysql
 service mysql start
 
+# Создаем базу zabbix с нужной кодировкой  
 mysql -uroot -e "create database zabbix character set utf8 collate utf8_bin;"
+
+# создаем пользователя zabbix и даем ему полные права на базу zabbix
 mysql -uroot -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';"
 
+# заливаем шаблон базы в новую базу данных zabbix
 mysql -uzabbix -p'zabbix' zabbix < /zabbix-4.0.14/database/mysql/schema.sql
 mysql -uzabbix -p'zabbix' zabbix < /zabbix-4.0.14/database/mysql/images.sql
 mysql -uzabbix -p'zabbix' zabbix < /zabbix-4.0.14/database/mysql/data.sql
 
+# службу mysql можно остановить
 service mysql stop
